27322b1f9b6ca36972ebdf6d404ffc8c54fa39ba
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-06-14 09:42:53 +0900

    add: TopPageDTO追加

---
+++ app/Http/DAO/TopPagePublishedArticleSummaryDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\DAO;
+
+class TopPagePublishedArticleSummaryDTO
+{
+    public string $thumbnail_url;
+
+    public function __construct(
+        public string $id,
+        public string $title,
+        public string $body,
+        string        $thumbnail_image_path,
+        public string $created_at,
+    )
+    {
+        $this->thumbnail_url = config('image.base_url') . $thumbnail_image_path;
+    }
+}
