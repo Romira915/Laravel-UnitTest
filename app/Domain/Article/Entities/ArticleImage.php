bcb76bfdb0744adbdeff147c06de86bc3f5b381f
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-06-17 12:53:12 +0900

    fix: IdをEntity内で生成する

--- app/Domain/Article/Entities/ArticleImage.php
+++ app/Domain/Article/Entities/ArticleImage.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Domain\Article\Entities;
 
+use Ramsey\Uuid\Uuid;
+
 readonly class ArticleImage
 {
     public function __construct(
@@ -13,6 +15,16 @@ readonly class ArticleImage
         private string $image_path,
     )
     {
+    }
+
+    public static function create(string $article_id, string $user_id, string $image_path): ArticleImage
+    {
+        return new self(
+            id: (string)Uuid::uuid7(),
+            article_id: $article_id,
+            user_id: $user_id,
+            image_path: $image_path
+        );
     }
 
     public function getId(): string
--- app/Domain/Article/Entities/PublishedArticle.php
+++ app/Domain/Article/Entities/PublishedArticle.php
@@ -7,6 +7,7 @@ namespace App\Domain\Article\Entities;
 
 use App\Domain\Article\Collection\ArticleImageList;
 use App\Domain\Article\Collection\ArticleTagList;
+use Ramsey\Uuid\Uuid;
 
 class PublishedArticle
 {
@@ -27,6 +28,27 @@ class PublishedArticle
         private ArticleImageList $images,
     )
     {
+    }
+
+    public static function create(
+        string $user_id,
+        string $title,
+        string $body,
+        string $thumbnail_path,
+        /** @var string[] $image_paths */
+        array $image_paths,
+    ): self
+    {
+        $article_id = (string)Uuid::uuid7();
+
+        return new self(
+            id: $article_id,
+            user_id: $user_id,
+            title: $title,
+            body: $body,
+            thumbnail_path: $thumbnail_path,
+            images: ArticleImageList::fromImagePathList($article_id, $user_id, $image_paths)
+        );
     }
 
     public function getId(): string
