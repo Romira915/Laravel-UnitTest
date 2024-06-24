d3e87a4f91055bc13199b84e18202f72666618e8
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-06-24 17:49:50 +0900

    fix: ArticleTagをReadOnlyクラスにする

--- app/Domain/Article/Entities/ArticleTag.php
+++ app/Domain/Article/Entities/ArticleTag.php
@@ -7,19 +7,28 @@ namespace App\Domain\Article\Entities;
 use App\Domain\Validator\TagValidator;
 use App\Utils\Uuid;
 
-class ArticleTag
+readonly class ArticleTag
 {
+    private string $id;
+    private string $tag_name;
+
     public function __construct(
         private string $user_id,
-        private string $tag_name,
-        private ?string $id = null,
+        string $tag_name,
         private ?string $article_id = null,
+        ?string $id = null,
     )
     {
-        if ($this->id === null) {
+        if ($id === null) {
             $this->id = Uuid::generate();
+        } else {
+            $this->id = $id;
         }
-        $this->setTag($tag_name);
+
+        if (!TagValidator::validate($tag_name)) {
+            throw new \InvalidArgumentException('Invalid tag name');
+        }
+        $this->tag_name = $tag_name;
     }
 
     public function getId(): string|null
@@ -36,16 +45,6 @@ class ArticleTag
     {
         return $this->user_id;
     }
-
-    private function setTag(string $tag_name): void
-    {
-        if (!TagValidator::validate($tag_name)) {
-            throw new \InvalidArgumentException('Invalid tag name');
-        }
-
-        $this->tag_name = $tag_name;
-    }
-
 
     public function getTag(): string
     {
